c5b9c6057900e641ffaaef4539a720606c13463f
cayandle <cayandle@live.com>
2021-09-07 17:59:40 -0400

    add 9/7/2021 challenge answers

---
+++ 09-07-2021/C#_Challenge.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Onboarding_Challenges
+{
+    class Challenge
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GiveMeSomething(Console.ReadLine()));
+        }
+
+        private static string GiveMeSomething(string phrase){
+            return "something " + phrase;
+        }
+    }
+}
